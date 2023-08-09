40aea5ef910202d02907fc49dc2905d718d26ea6
Marino Di Clemente <kernelfolla@gmail.com>
2023-08-09 19:46:40 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # nix-config
-My personal nix-darwin configuration
+
+*My personal nix-darwin configuration*
+
+## Why
+
+Nix reminds me of Gentoo. I spent a part of my life getting Gentoo to work, so why not spend the rest with Nix?
+Jokes aside, I've always did (tried) to keep most of my setup under version control, did symlinks, scripts, Brewfile, 
+
+I can perform this sort of magic thanks to amazing people who shared their own experiences and made configurations public ( [just search nix-config in github](https://github.com/search?q=nix-config+darwin&type=repositories)), so I feel obliged to do the same.
+
+## What
+
+- **[nix](https://nixos.org/)**: A package manager and a distribution-independent package manager system that focuses on declarative and functional package management.
+- **[nix-darwin](https://github.com/LnL7/nix-darwin)**: An extension of Nix that focuses on managing the configuration of macOS systems.
+- **[home-manager](https://github.com/nix-community/home-manager)**: An add-on for Nix that focuses on managing user-level environments.
+
+## How
+
+TBD
+
+## Credits
+
+A big thank you to zmre for sharing their nix-config repository and providing inspiration for this configuration!
+
+- **ZMRE Tutorial**: [Watch the YouTube Video](https://www.youtube.com/watch?v=LE5JR4JcvMg)
+- **ZMRE personal configuration**: [zmre/nix-config Repository](https://github.com/zmre/nix-config)
+
---
+++ hosts/Marino-MM2/default.nix
@@ -0,0 +1,48 @@
+{ pkgs, ... }: {
+  # here go the darwin preferences and config items
+  programs.zsh.enable = true;
+  environment = {
+    shells = with pkgs; [ bash zsh ];
+    loginShell = pkgs.zsh;
+    systemPackages = [ pkgs.coreutils ];
+    systemPath = [ "/opt/homebrew/bin" ];
+    pathsToLink = [ "/Applications" ];
+  };
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = true;
+  fonts.fontDir.enable = true; # DANGER
+  fonts.fonts = [ (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; }) ];
+  services.nix-daemon.enable = true;
+  system.defaults = {
+    finder.AppleShowAllExtensions = true;
+    finder._FXShowPosixPathInTitle = true;
+    dock.autohide = true;
+    NSGlobalDomain.AppleShowAllExtensions = true;
+    NSGlobalDomain.InitialKeyRepeat = 14;
+    NSGlobalDomain.KeyRepeat = 1;
+    NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+  };
+  # backwards compat; don't change
+  system.stateVersion = 4;
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+    caskArgs.no_quarantine = true;
+    global.brewfile = true;
+    masApps = { };
+    casks = [
+      "dropbox"
+      "jetbrains-toolbox"
+      "iterm2"
+      "logi-options-plus"
+      "orbstack"
+      "notion"
+    ];
+    taps = [ "fujiapple852/trippy" ];
+    brews = [ "trippy" ];
+  };
+}
